464ef2f5e9f543240cde02a8f453d763eff55cb6
gustavandoff <gustav.andoff@gmail.com>
2024-01-14 17:44:09 +0100

    Page to list cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,40 @@
 <script>
+	import ao5Icon from '$lib/images/ao5icon.png';
+	import ao12Icon from '$lib/images/ao12icon.png';
+	import ao100Icon from '$lib/images/ao100icon.png';
+
+	import BaseCard from './BaseCard.svelte';
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-
+<div class="stat-list">
+	<BaseCard
+		route="/ao5"
+		imgSrc={ao5Icon}
+		imgAlt="Ao5"
+		title="Ao5 over time"
+		text="Shows a graph of all the averages of 5"
+	/>
+	<BaseCard
+		route="/ao12"
+		imgSrc={ao12Icon}
+		imgAlt="Ao12"
+		title="Ao12 over time"
+		text="Shows a graph of all the averages of 12"
+	/>
+	<BaseCard
+		route="/ao100"
+		imgSrc={ao100Icon}
+		imgAlt="Ao100"
+		title="Ao100 over time"
+		text="Shows a graph of all the averages of 100"
+	/>
+</div>
 
 <style>
+	.stat-list {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 10px;
+	}
 </style>
