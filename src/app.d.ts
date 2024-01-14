430fcfb5e07e025300e07cea6f3ff14b7cb104ea
gustavandoff <gustav.andoff@gmail.com>
2024-01-14 17:44:48 +0100

    Create store to manage times and stats

--- src/app.d.ts
+++ src/app.d.ts
@@ -10,4 +10,33 @@ declare global {
 	}
 }
 
-export {};
+interface Solve {
+	time: string;
+	timeInMillis: number;
+	scramble: string;
+	comment: string;
+	date: string;
+}
+
+interface Session {
+	sessionName: string;
+	solves: Solve[];
+}
+
+interface Average {
+	time: string;
+	timeInMillis: number;
+	dateOfLast: string;
+	solves: Solve[];
+}
+
+interface SessionSolve {
+	0: number[]; // time in milliseconds
+	1: string;   // scramble
+	2: string;   // comment
+	3: number;   // date
+}
+
+interface csSessionData {
+	name: string | number;
+}
---
+++ src/lib/solves.ts
@@ -0,0 +1,101 @@
+import { writable, derived, get, type Readable } from 'svelte/store';
+import { convertMilliseconds, convertDate, calcAo5 } from './utils';
+
+const rawCSTimerData = writable<any>(null);
+const allAo5 = writable<Session[]>([]);
+const csSessionData = writable<csSessionData[]>([]);
+
+export const setRawCSTimerData = (data: any) => {
+	console.log('setRawCSTimerData');
+	rawCSTimerData.set(data);
+	console.log('rawCSTimerData: ', data);
+
+	csSessionData.set(
+		Object.values(JSON.parse(data.properties.sessionData)).map((session: any) => session.name)
+	);
+	console.log(
+		'csSessionData: ',
+		Object.values(JSON.parse(data.properties.sessionData)).map((session: any) => session.name)
+	);
+};
+
+const formatedCSTimerData: Readable<Session[]> = derived(
+	[csSessionData, rawCSTimerData],
+	([$csSessionData, $rawCSTimerData], set) => {
+		let formatedData: Session[] = [];
+
+		for (let i = 1; i < $csSessionData.length; i++) {
+			const sessionName = $csSessionData[i].name;
+			const session = $rawCSTimerData['session' + i];
+
+			let formatedSession: Solve[] = [];
+
+			session.forEach((solve: SessionSolve) => {
+				formatedSession.push({
+					time: convertMilliseconds(solve[0][1]),
+					timeInMillis: solve[0][1],
+					scramble: solve[1],
+					comment: solve[2],
+					date: convertDate(solve[3])
+				});
+			});
+
+			if (formatedSession.length !== 0) {
+				formatedData.push({
+					sessionName: '' + sessionName,
+					solves: formatedSession
+				});
+			}
+		}
+		set(formatedData);
+	}
+);
+
+const allSinglePBs: Readable<Session[]> = derived(formatedCSTimerData, ($formatedCSTimerData, set) => {
+	const data = $formatedCSTimerData;
+	let allPBs: Session[] = [];
+	$formatedCSTimerData.forEach((session) => {
+		let pbsInThisSession: Solve[] = [];
+		let currentPB: Solve | null = null;
+
+		session.solves.forEach((solve) => {
+			if (!currentPB || solve.timeInMillis < currentPB.timeInMillis) {
+				pbsInThisSession.push(solve);
+				currentPB = solve;
+			}
+		});
+
+		allPBs.push({
+			sessionName: session.sessionName,
+			solves: pbsInThisSession
+		});
+	});
+
+	set(allPBs);
+});
+
+const getAllAo5 = derived(formatedCSTimerData, ($formatedCSTimerData, set) => {
+	let tempAllAo5: Session[] = [];
+	$formatedCSTimerData.forEach((session) => {
+		let ao5sInThisSession: any[] = [];
+		let currentAo5: any = null;
+
+		for (let i = 0; i < session.solves.length - 5; i++) {
+			currentAo5 = calcAo5([
+				session.solves[i],
+				session.solves[i + 1],
+				session.solves[i + 2],
+				session.solves[i + 3],
+				session.solves[i + 4]
+			]);
+			ao5sInThisSession.push(currentAo5);
+		}
+
+		tempAllAo5.push({
+			sessionName: session.sessionName,
+			solves: ao5sInThisSession
+		});
+	});
+
+	return set(tempAllAo5);
+});
