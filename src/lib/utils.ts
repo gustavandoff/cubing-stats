52c87b83e862f76e31ef92c5a49aa6e950ea3a3e
gustavandoff <gustav.andoff@gmail.com>
2024-01-16 17:09:49 +0100

    Function for getting list of averages from list of solves

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,38 +1,71 @@
 export const convertMilliseconds = (milliseconds: number): string => {
 	const totalSeconds = Math.floor(milliseconds / 1000);
-    const minutes = Math.floor(totalSeconds / 60);
-    const seconds = totalSeconds % 60;
-    const hundredths = Math.floor((milliseconds % 1000) / 10);
+	const minutes = Math.floor(totalSeconds / 60);
+	const seconds = totalSeconds % 60;
+	const hundredths = Math.floor((milliseconds % 1000) / 10);
 
-    if (minutes > 0) {
-        return `${minutes}:${seconds.toString().padStart(2, '0')}.${hundredths.toString().padStart(2, '0')}`;
-    } else {
-        return `${seconds}.${hundredths.toString().padStart(2, '0')}`;
-    }
+	if (minutes > 0) {
+		return `${minutes}:${seconds.toString().padStart(2, '0')}.${hundredths
+			.toString()
+			.padStart(2, '0')}`;
+	} else {
+		return `${seconds}.${hundredths.toString().padStart(2, '0')}`;
+	}
 };
 
 export const convertDate = (s: number): string => {
-    const date = new Date(s * 1000);
-    return date.toLocaleDateString();
+	const date = new Date(s * 1000);
+	return date.toLocaleDateString();
 };
 
-export const calcAvg = (solves: Solve[], removedCount: number): Solve => {
+const calcAvg = (solves: Solve[], removedCount: number): Solve => {
 	if (2 * removedCount > solves.length - 1) {
 		throw new Error('Too many solves removed');
 	}
 	solves.sort((a, b) => b.timeInMillis - a.timeInMillis);
-	
+
 	solves.splice(0, removedCount);
 	solves.splice(solves.length - removedCount, removedCount);
 
 	return calcMean(solves);
 };
 
-export const calcMean = (solves: Solve[]): Solve => {
+const calcMean = (solves: Solve[]): Solve => {
 	const avgTimeInMillis = solves.reduce((acc, curr) => acc + curr.timeInMillis, 0) / solves.length;
 	return {
 		time: convertMilliseconds(avgTimeInMillis),
 		timeInMillis: avgTimeInMillis,
 		date: solves[0].date
 	};
+};
+
+export const getAllAoX = (x: number, solves: Solve[]): Solve[] => {
+	let removedCount = 0;
+	switch (x) {
+		case 5:
+			removedCount = 1;
+			break;
+		case 12:
+			removedCount = 1;
+			break;
+		case 25:
+			removedCount = 3;
+			break;
+		case 50:
+			removedCount = 3;
+			break;
+		default:
+			if (x % 100 === 0) {
+				removedCount = (x / 100) * 5;
+			} else {
+				throw new Error('Invalid AoX');
+			}
+			break;
+	}
+
+	const aoXs: Solve[] = [];
+	for (let i = 0; i < solves.length - x; i++) {
+		aoXs.push(calcAvg(solves.slice(i, i + x), removedCount));
+	}
+	return aoXs;
 };
