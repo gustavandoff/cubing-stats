dbae700e31dde6059333d2f9d851dcce584bfec8
gustavandoff <gustav.andoff@gmail.com>
2024-01-16 14:29:18 +0100

    Remove calcAo5 and add general calcAverage

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -16,25 +16,23 @@ export const convertDate = (s: number): string => {
     return date.toLocaleDateString();
 };
 
-export const calcAo5 = (solves: Solve[]): Solve => {
-	const unsortedSolves = [...solves];
-	solves.sort((a, b) => a.timeInMillis - b.timeInMillis);
-	const avgTimeInMillis =
-		(solves[1].timeInMillis + solves[2].timeInMillis + solves[3].timeInMillis) / 3;
-	return {
-		time: convertMilliseconds(avgTimeInMillis),
-		timeInMillis: avgTimeInMillis,
-		date: solves[4].date
-	};
+export const calcAvg = (solves: Solve[], removedCount: number): Solve => {
+	if (2 * removedCount > solves.length - 1) {
+		throw new Error('Too many solves removed');
+	}
+	solves.sort((a, b) => b.timeInMillis - a.timeInMillis);
+	
+	solves.splice(0, removedCount);
+	solves.splice(solves.length - removedCount, removedCount);
+
+	return calcMean(solves);
 };
 
 export const calcMean = (solves: Solve[]): Solve => {
-	const unsortedSolves = [...solves];
-	solves.sort((a, b) => b.timeInMillis - a.timeInMillis);
 	const avgTimeInMillis = solves.reduce((acc, curr) => acc + curr.timeInMillis, 0) / solves.length;
 	return {
 		time: convertMilliseconds(avgTimeInMillis),
 		timeInMillis: avgTimeInMillis,
 		date: solves[0].date
 	};
-}
+};
