21346e66c2e61f7f75a28844f5ee37927ce02c33
gustavandoff <gustav.andoff@gmail.com>
2024-01-25 03:11:13 +0100

    Change title size

--- src/lib/components/IconCard.svelte
+++ src/lib/components/IconCard.svelte
@@ -10,7 +10,7 @@
         {@html svg}
     </span>
 	<div class="card-body">
-		<h5 class="card-title">{title}</h5>
+		<h2 class="card-title">{title}</h2>
 		<p class="card-text">{text}</p>
 	</div>
 </a>
@@ -41,7 +41,7 @@
 		margin: 10px 0 0 0;
 		font-weight: 1000;
 		line-height: 1.2;
-		font-size: 1.25rem;
+		font-size: 2rem;
 		color: #ffffff;
 	}
 
