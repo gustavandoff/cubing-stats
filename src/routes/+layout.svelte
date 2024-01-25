37ae1ff8dc2b92aced33aa0415d469235767ae6b
gustavandoff <gustav.andoff@gmail.com>
2024-01-25 01:53:02 +0100

    Remove AoX routing cards. Add routing buttons for /graph and /list. Move routing buttons from layout to page.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,6 @@
 	
 	import '@fortawesome/fontawesome-free/css/all.min.css'
 
-	import ao5Icon from '$lib/images/ao5icon.png';
-	import ao12Icon from '$lib/images/ao12icon.png';
-	import ao25Icon from '$lib/images/ao25icon.png';
-	import ao50Icon from '$lib/images/ao50icon.png';
-	import ao100Icon from '$lib/images/ao100icon.png';
-
-	import BaseCard from '$lib/components/BaseCard.svelte';
 	import LoadTimes from '$lib/components/LoadTimes.svelte';
 
 	import { rawCSTimerData, setRawCSTimerData } from '$lib/solves';
@@ -19,62 +12,10 @@
 	}
 
 	$: showLoadTimes = browser && !localStorage.getItem('rawCSTimerData');
-	$: currentUrl = browser ? window.location.origin : '';
 </script>
 
 {#if showLoadTimes}
 	<LoadTimes />
 {/if}
 
-<div class="stat-list" class:inactive={showLoadTimes}>
-	<BaseCard
-		route={currentUrl + '/graph/ao5'}
-		imgSrc={ao5Icon}
-		imgAlt="Ao5"
-		title="Ao5 over time"
-		text="Shows a graph of all the averages of 5"
-	/>
-	<BaseCard
-		route={currentUrl + '/graph/ao12'}
-		imgSrc={ao12Icon}
-		imgAlt="Ao12"
-		title="Ao12 over time"
-		text="Shows a graph of all the averages of 12"
-	/>
-	<BaseCard
-		route={currentUrl + '/graph/ao25'}
-		imgSrc={ao25Icon}
-		imgAlt="Ao25"
-		title="Ao25 over time"
-		text="Shows a graph of all the averages of 25"
-	/>
-	<BaseCard
-		route={currentUrl + '/graph/ao50'}
-		imgSrc={ao50Icon}
-		imgAlt="Ao50"
-		title="Ao50 over time"
-		text="Shows a graph of all the averages of 50"
-	/>
-	<BaseCard
-		route={currentUrl + '/graph/ao100'}
-		imgSrc={ao100Icon}
-		imgAlt="Ao100"
-		title="Ao100 over time"
-		text="Shows a graph of all the averages of 100"
-	/>
-</div>
-
 <slot />
-
-<style>
-	.stat-list {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		gap: 10px;
-	}
-
-	.stat-list.inactive {
-		pointer-events: none;
-	}
-</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+    
+	import IconCard from '$lib/components/IconCard.svelte';
+
+	import { rawCSTimerData, setRawCSTimerData } from '$lib/solves';
+
+	$: if (browser && localStorage.getItem('rawCSTimerData') && !$rawCSTimerData) {
+		setRawCSTimerData(JSON.parse(localStorage.getItem('rawCSTimerData') as string));
+	}
+
+	$: showLoadTimes = browser && !localStorage.getItem('rawCSTimerData');
+	$: currentUrl = browser ? window.location.origin : '';
+</script>
+
+<div class="stat-list" class:inactive={showLoadTimes}>
+    <IconCard
+        route='/graph/'
+        svg='<i class="fa-solid fa-chart-line"></i>'
+        title="Line graphs"
+        text="Line graphs of all your solves, averages or PBs"
+    />
+    <IconCard
+        route='/list/'
+        svg='<i class="fa-solid fa-list-ul"></i>'
+        title="List"
+        text="List all your solves, averages or PBs"
+    />
+</div>
+
+<style>
+	.stat-list {
+		display: flex;
+        flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: 10px;
+	}
+
+	.stat-list.inactive {
+		pointer-events: none;
+	}
+</style>
