3926cbb6a35e081a3eba5c09f88cd3f20a2f823c
gustavandoff <gustav.andoff@gmail.com>
2024-10-22 20:21:57 +0200

    Open SolveInformationPopup when time in list is pressed

--- src/lib/components/timer/SolvesListItem.svelte
+++ src/lib/components/timer/SolvesListItem.svelte
@@ -1,11 +1,31 @@
 <script lang="ts">
+  import SolveInformationPopup from "./SolveInformationPopup.svelte";
+
   export let solve: Solve;
-</script>
+
+  let showPopup = false;
+
+  let popupParent: HTMLElement;
+
+  function onClose() {
+    showPopup = false;
+  }
+
+  function openPopup() {
+    showPopup = true;
+  }
+  
+  </script>
 
 <li>
-  <div class="solve-time-container">
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
+  <div class="solve-time-container" on:click={openPopup} title="More info" bind:this={popupParent}>
     <span>{solve.time}</span>
     <!-- <span>{solve.date}</span> -->
+    {#if showPopup}
+      <SolveInformationPopup {solve} {onClose} {popupParent} />
+    {/if}
   </div>
 </li>
 
@@ -17,9 +37,14 @@
   }
 
   .solve-time-container {
+    cursor: pointer;
     width: 100%;
     display: flex;
     justify-content: end; 
     gap: 1.5rem;
+
+    &:hover {
+			color: var(--hovered-color);
+		}
   }
 </style>
