8111a46b23576d69bc101f921e7436054e534cc3
gustavandoff <gustav.andoff@gmail.com>
2024-10-26 00:28:46 +0200

    Push solves to period arrays in one loop instead of many loops

--- src/lib/components/timer/SolvesList.svelte
+++ src/lib/components/timer/SolvesList.svelte
@@ -12,71 +12,51 @@
 
 	$: {
 		if ($formattedCSTimerData[0]?.solves) {
-			todaySolves = $formattedCSTimerData[0].solves.filter((solve) => {
+			const solvesList = $formattedCSTimerData[0].solves;
+			solvesList.reverse();
+
+			todaySolves = [];
+			yesterDaySolves = [];
+			thisWeekSolves = [];
+			thisMonthSolves = [];
+			thisYearSolves = [];
+			allTimeSolves = [];
+
+			const today = new Date();
+			const day = today.getDay();
+			const diff = today.getDate() - day + (day === 0 ? -6 : 1);
+			const weekStart = new Date(today.setDate(diff));
+
+			today.setDate(new Date().getDate());
+			
+			solvesList.forEach(solve => {
 				const solveDate = new Date(solve.date);
-				const today = new Date();
-				return (
+
+				if (
 					solveDate.getDate() === today.getDate() &&
 					solveDate.getMonth() === today.getMonth() &&
 					solveDate.getFullYear() === today.getFullYear()
-				);
-			}).reverse();
-
-			yesterDaySolves = $formattedCSTimerData[0].solves.filter((solve) => {
-				const solveDate = new Date(solve.date);
-				const yesterday = new Date();
-				yesterday.setDate(yesterday.getDate() - 1);
-				return (
-					solveDate.getDate() === yesterday.getDate() &&
-					solveDate.getMonth() === yesterday.getMonth() &&
-					solveDate.getFullYear() === yesterday.getFullYear()
-				);
-			}).reverse();;
-
-			thisWeekSolves = $formattedCSTimerData[0].solves.filter((solve) => {
-				const solveDate = new Date(solve.date);
-				const today = new Date();
-				const day = today.getDay();
-				const diff = today.getDate() - day + (day === 0 ? -6 : 1);
-				const weekStart = new Date(today.setDate(diff));
-				return (
-					solveDate >= weekStart && !todaySolves.includes(solve) && !yesterDaySolves.includes(solve)
-				);
-			}).reverse();;
-
-			thisMonthSolves = $formattedCSTimerData[0].solves.filter((solve) => {
-				const solveDate = new Date(solve.date);
-				const today = new Date();
-				return (
+				) {
+					todaySolves.push(solve);
+				} else if (
+					solveDate.getDate() === today.getDate() - 1 &&
 					solveDate.getMonth() === today.getMonth() &&
-					solveDate.getFullYear() === today.getFullYear() &&
-					!todaySolves.includes(solve) &&
-					!yesterDaySolves.includes(solve) &&
-					!thisWeekSolves.includes(solve)
-				);
-			}).reverse();;
-
-			thisYearSolves = $formattedCSTimerData[0].solves.filter((solve) => {
-				const solveDate = new Date(solve.date);
-				const today = new Date();
-				return (
-					solveDate.getFullYear() === today.getFullYear() &&
-					!todaySolves.includes(solve) &&
-					!yesterDaySolves.includes(solve) &&
-					!thisWeekSolves.includes(solve) &&
-					!thisMonthSolves.includes(solve)
-				);
-			}).reverse();;
-
-			allTimeSolves = $formattedCSTimerData[0].solves.filter((solve) => {
-				return (
-					!todaySolves.includes(solve) &&
-					!yesterDaySolves.includes(solve) &&
-					!thisWeekSolves.includes(solve) &&
-					!thisMonthSolves.includes(solve) &&
-					!thisYearSolves.includes(solve)
-				);
-			}).reverse();;
+					solveDate.getFullYear() === today.getFullYear()
+				) {
+					yesterDaySolves.push(solve);
+				} else if (solveDate >= weekStart) {
+					thisWeekSolves.push(solve);
+				} else if (
+					solveDate.getMonth() === today.getMonth() &&
+					solveDate.getFullYear() === today.getFullYear()
+				) {
+					thisMonthSolves.push(solve);
+				} else if (solveDate.getFullYear() === today.getFullYear()) {
+					thisYearSolves.push(solve);
+				} else {
+					allTimeSolves.push(solve);
+				}
+			});
 		}
 	}
 </script>
