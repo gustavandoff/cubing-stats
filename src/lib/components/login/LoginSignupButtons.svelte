a760e2478828ac94e348258c58ff7f0de912ab07
gustavandoff <gustav.andoff@gmail.com>
2024-10-30 23:50:08 +0100

    Dispatch events then clicking buttons

--- src/lib/components/login/LoginSignupButtons.svelte
+++ src/lib/components/login/LoginSignupButtons.svelte
@@ -1,12 +1,24 @@
 <script lang="ts">
-
   import Button from "../Button.svelte";
+
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  function handleLogin() {
+    dispatch('login');
+  }
+
+  function handleSignup() {
+    dispatch('signup');
+  }
+
 
 </script>
 
 <div>
-  <Button>Log in</Button>
-  <Button>Sign up</Button>
+  <Button on:click={handleLogin}>Log in</Button>
+  <Button on:click={handleSignup}>Sign up</Button>
 </div>
 
 <style>
