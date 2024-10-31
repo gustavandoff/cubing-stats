fd2d068d7a3dd6f9d5875e7af2be56c71ea8e72b
gustavandoff <gustav.andoff@gmail.com>
2024-10-31 12:41:59 +0100

    Add minute colon and second point signs to text. Align input to the right.

--- src/lib/components/timer/TypeTimer.svelte
+++ src/lib/components/timer/TypeTimer.svelte
@@ -26,22 +26,46 @@
     }
 	}
 
-  $: inputTime = inputTime.replace(/\D/g, '');
+  $: {
+    inputTime = inputTime.replace(/\D/g, '');
+    if (inputTime.length > 4) {
+      inputTime = `${inputTime.slice(0, -4)}:${inputTime.slice(-4, -2)}.${inputTime.slice(-2)}`;
+    } else if (inputTime.length > 2) {
+      inputTime = `${inputTime.slice(0, -2)}.${inputTime.slice(-2)}`;
+    }
+  }
 
 </script>
 
 
-<div>
-	<input placeholder="00" type="text" class="time" bind:value={inputTime} on:keydown={handleKeydown}>
+<div class="time">
+  <input placeholder="0:00.00" type="text" bind:value={inputTime} on:keydown={handleKeydown}>
 </div>
 
 <style>
 	.time {
+    box-sizing: border-box;
 		font-size: 10rem;
 		font-weight: 500;
-		text-align: center;
+		text-align: end;
 		margin: 0;
     width: 50rem;
+    border: 1px solid var(--greyed-color);
+    border-radius: 5px;
+    display: flex;
+    justify-content: center;
+
+    & input {
+      box-sizing: border-box;
+      background-color: transparent;
+      border: none;
+      font-size: 10rem;
+      font-weight: 500;
+      color: #fff;
+      width: 80%;
+      text-align: end;
+      outline: none;
+    }
 	}
 
 </style>
