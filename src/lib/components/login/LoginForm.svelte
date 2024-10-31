0a23c85e3cada6c2ed40a08b014d968fdc0d9a15
gustavandoff <gustav.andoff@gmail.com>
2024-10-31 22:15:23 +0100

    Add login form component

---
+++ src/lib/components/login/LoginForm.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+import Form from "../Form/Form.svelte";
+import FormTitle from "../Form/FormTitle.svelte";
+import FormInput from "../Form/FormInput.svelte";
+import FormButton from "../Form/FormButton.svelte";
+
+import Modal from "../Modal.svelte";
+
+import { createEventDispatcher } from 'svelte';
+
+const dispatch = createEventDispatcher();
+
+let username = '';
+let password = '';
+
+function handleClose() {
+  dispatch('close');
+}
+
+function handleClick() {
+  console.log('clicked');
+  console.log('username:', username);
+  console.log('password:', password);
+}
+
+</script>
+
+<Modal on:close={handleClose}>
+  <Form>
+    <FormTitle>Login</FormTitle>
+    <FormInput bind:value={username} type="text" placeholder="Username" required />
+    <FormInput bind:value={password} type="password" placeholder="Password" required />
+    <FormButton type="submit" on:click={handleClick}>Login</FormButton>
+  </Form>
+</Modal>
