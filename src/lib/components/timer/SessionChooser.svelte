76d018602e6198eb4e0c266a673650b3c76282f9
gustavandoff <gustav.andoff@gmail.com>
2024-11-01 12:58:26 +0100

    Component for choosing session

---
+++ src/lib/components/timer/SessionChooser.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { formattedCSTimerData, currentSession } from '$lib/solves';
+
+  let session = $currentSession;
+
+  function handleSessionChange(e: Event) {
+    const target = e.target as HTMLSelectElement;
+    session = target.value;
+    currentSession.set(session);
+  }
+</script>
+
+<select bind:value={session} on:change={handleSessionChange}>
+  {#each $formattedCSTimerData as session}
+    <option value={session.sessionName}>{session.sessionName}</option>
+  {/each}
+</select>
