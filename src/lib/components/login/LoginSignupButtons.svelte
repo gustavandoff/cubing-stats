18512efa7e98c12e7ccb17bbcbd4675ac7d1b2ef
gustavandoff <gustav.andoff@gmail.com>
2024-11-02 03:56:13 +0100

    Add button to reupload csTimer file

--- src/lib/components/login/LoginSignupButtons.svelte
+++ src/lib/components/login/LoginSignupButtons.svelte
@@ -13,12 +13,19 @@
     dispatch('signup');
   }
 
+  function handleUpload() {
+    dispatch('upload');
+  }
+
 
 </script>
 
 <div>
   <Button on:click={handleLogin}>Log in</Button>
   <Button on:click={handleSignup}>Sign up</Button>
+  <Button on:click={handleUpload}>
+    <i class="fa-solid fa-upload"></i>
+  </Button>
 </div>
 
 <style>
@@ -28,5 +35,9 @@
     position: absolute;
     right: 1rem;
     top: 1rem;
+  }
+
+  .fa-upload {
+    font-size: 1.5rem;
   }
 </style>
